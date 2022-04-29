70aa2a0aa83e0d24262c80c78e7eca409c9d0583
Perfect_03 <zlobin_gg52926@mail.ru>
2022-04-29 15:47:35 +0700

    определение класса FileAbout (дающего информацию о файле) и паттерна Наблюдателя

---
+++ fileAbout.h
@@ -0,0 +1,16 @@
+﻿#ifndef FILEABOUT_H
+#define FILEABOUT_H
+
+#include <cstddef>
+class FileAbout {
+    bool operator!= (const FileAbout& other) 
+    {
+        if(this == &other) return false;
+        return exists != other.exists || size != other.size;
+    }
+ 
+    bool exists;
+    size_t size;
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,67 +1,12 @@
 ﻿#include <QCoreApplication>
 #include <iostream>
-#include "stack.h"
-#include "person_keeper.h"
-#include "person.h"
+#include "fileAbout.h"
+#include "observable.h"
 
-int main(int argc, char *argv[])
-{
-   QCoreApplication a(argc, argv);
+int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
 
-   setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "Russian");
 
-    Stack<const char *> t;
-    t.Push("+");
-    t.Push("+");
-    t.Push("B");
-    cout << "Size of stack before node removal: " << t.Size() << endl;
-    t.Pop();
-    cout << "Size of stack after node removal: " << t.Size() << endl;
-    t.Push("C");
-
-
-    cout << "Elements of stack: " << endl;
-    while (true)
-    {
-        cout << t.Pop() << endl;
-    }
-
-    Stack<int> z;
-    z.Push(2);
-    while (true)
-    {
-        cout << z.Pop() << endl;
-    }
-
-    // ПРОВЕРКА ИСКЛЮЧЕНИЯ
-
-    Stack<int> w;
-    w.Pop(); // пробуем извлечь элемент из пустого стека - получаем исключение
-
-    //2-я часть
-    // все примеры нужно проверять по отдельности ввиду особенностей Singleton-класса.
-    auto& b = PersonKeeper::Instance();
-    b.ReadPersons("C:/Users/User/Documents/GT_1/build-GT_1-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/persons/1.txt");
-    cout << endl << "Count of names: " << b.Size() << endl;
-    b.WritePersons("C:/Users/User/Documents/GT_1/build-GT_1-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/for_write/1.txt");
-
-    auto& c = PersonKeeper::Instance();
-    c.ReadPersons("C:/Users/User/Documents/GT_1/build-GT_1-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/persons/2.txt");
-    c.WritePersons("C:/Users/User/Documents/GT_1/build-GT_1-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/for_write/2.txt");
-
-    auto& e = PersonKeeper::Instance(); // пример, когда строка с ФИО содержит 2 слова
-    e.ReadPersons("C:/Users/User/Documents/GT_1/build-GT_1-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/persons/4.txt");
-    e.WritePersons("C:/Users/User/Documents/GT_1/build-GT_1-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/for_write/4.txt");
-
-    auto& d = PersonKeeper::Instance(); // пример, когда строка с ФИО содержит 1 слово
-    d.ReadPersons("C:/Users/User/Documents/GT_1/build-GT_1-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/persons/3.txt");
-    d.WritePersons("C:/Users/User/Documents/GT_1/build-GT_1-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/for_write/3.txt");
-
-    auto& f = PersonKeeper::Instance(); // пример, когда строка с ФИО содержит 4 слова
-    f.ReadPersons("C:/Users/User/Documents/GT_1/build-GT_1-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/persons/5.txt");
-    f.WritePersons("C:/Users/User/Documents/GT_1/build-GT_1-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/for_write/5.txt");
-
-    auto& g = PersonKeeper::Instance(); // пример с пустой строкой между именами
-    g.ReadPersons("C:/Users/User/Documents/GT_1/build-GT_1-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/persons/6.txt");
-    g.WritePersons("C:/Users/User/Documents/GT_1/build-GT_1-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/for_write/6.txt");
+    return 0;
 }
